6418eba4c6b39753a197cb1c4e944c3ae451a23f
Ash Lai <z0975201508@gmail.com>
2021-09-23 16:07:02 +0800

    Count Unique Values Solution Sample

--- sum.js
+++ sum.js
@@ -60,7 +60,7 @@ console.log(result2);
 // Understand Problem -> 編寫一個函式, 接受一個排列好的陣列做為參數, 接受負數為陣列元素, 但仍需要為排列好的陣列, 回傳總共出現幾個獨特數字的次數
 // 解題思路 -> 以 Two Pointers 方式解題, 設第一點為首點 i, 第二點為鄰近的下一點 j, 先排除空陣列的情況回傳 0, 透過 while 迴圈遍歷整個排列好的陣列
 // 固定 i 點、 j 點, 根據兩個位置的值相不相同來決定要移動的點, 若相同 -> j 遞增 i 不動, 若不同 -> i 遞增並將目前 i 點位置的值替換為目前 j 點位置的值
-// 最後因為有經過替換, 則從最開始到 i 點目前的陣列為排列好的陣列且不重複, 題目答案需要獨特數字出現的"次數", 這邊只要回傳 i + 1 就可以得到該次數 
+// 最後因為有經過替換, 則從最開始到 i 點目前的陣列為排列好的陣列且不重複, 題目答案需要獨特數字出現的"次數", 這邊只要回傳 i + 1 就可以得到該次數
 
 // 範例:
 
@@ -84,7 +84,7 @@ console.log(result2);
 // [1, 2, 3, 3]
 //           j
 
-// 因為 j 到陣列尾部了, 當前 i 的 index 為 2 則計算目前 i + 1 即可以得出總共出現幾個獨特數字次數 
+// 因為 j 到陣列尾部了, 當前 i 的 index 為 2 則計算目前 i + 1 即可以得出總共出現幾個獨特數字次數
 
 function countUniqueValues(sortArr) {
   // 先排除無元素
@@ -110,3 +110,26 @@ function countUniqueValues(sortArr) {
 
 const result3 = countUniqueValues([-1, 1, 1, 2, 3, 3, 4, 4, 5, 6]);
 console.log(result3);
+
+// Challenge - countUniqueValues (題解) Time complexity - O(n) & Space complexity O(1)
+// 解題思路 -> 基本上一樣差別在於我原本使用while跑迴圈, 題解使用for迴圈, 且不須多寫一個判斷, 解題核心沒變
+
+function countUniqueValues1(sortArr) {
+  // 先排除無元素
+  if (!sortArr.length) return 0;
+  // 設i點
+  let i = 0;
+  // j點設置for迴圈內, 一樣的條件 -> 小於陣列長度
+  for (let j = 1; j < sortArr.length; j++) {
+    // 若遇不相同, 一樣i遞增且替換i點目前的值為j點目前的值, 不相等的情況j會自己再for回圈內遞增
+    if (sortArr[i] !== sortArr[j]) {
+      i++;
+      sortArr[i] = sortArr[j];
+    }
+  }
+  // 回傳目前 i 的位置加1, 因為 i 為 index 從 0 開始算
+  return i + 1;
+}
+
+const result4 = countUniqueValues1([-1, 1, 1, 2, 3, 3, 4, 4, 5, 6]);
+console.log(result4);

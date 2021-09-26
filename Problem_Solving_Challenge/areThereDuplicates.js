c497a03f0bae027660fde56076d66977c9322448
Ash Lai <z0975201508@gmail.com>
2021-09-26 17:01:12 +0800

    Code Exercise 4: Frequency Counter / Mutiple Pointers - areThereDuplicates (Mutiple Pointers Solution)

--- Problem_Solving_Challenge/areThereDuplicates.js
+++ Problem_Solving_Challenge/areThereDuplicates.js
@@ -22,3 +22,24 @@ function areThereDuplicates(arg1, arg2, arg3) {
 
 const result = areThereDuplicates(1, 1, 3);
 console.log(result);
+
+// mutiple pointers pattern solution
+// 解題思路 - 用兩個 pointers 的方式去倆倆比對, 過程中遇到卡很久的問題出在sort()這個原型方法
+// 因為給定測驗的 input 中有字串, 原寫法: sort((a, b) => a - b) 指對數字生效排序, 字串 input: 'a', 'b', 'c', 'a' 卻不生效, 導致卡了很久
+// 後來只能使用 default 的 sort() 不加參數, 完成正常的由小到大排列
+
+function areThereDuplicates1(arg1, arg2, arg3) {
+  let argArr = Array.from(arguments).sort();
+  let i = 0;
+  for (let j = 1; j < argArr.length; j++) {
+    if (argArr[i] === argArr[j]) {
+      return true;
+    }
+    i++;
+  }
+  return false;
+}
+
+const result1 = areThereDuplicates1('a', 'b', 'c', 'a');
+console.log(result1);
+
